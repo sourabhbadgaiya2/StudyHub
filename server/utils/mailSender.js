bf3c5024c860d093f0da46c4136fafb92f9c1e07
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2025-10-07 10:06:40 +0530

    update sb

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -31,34 +31,27 @@
 
 // module.exports = mailSender;
 
-const nodemailer = require("nodemailer");
+// mailSender.js
+import { Resend } from "resend";
+
+// Initialize Resend with API key from environment variable
+const resend = new Resend("re_Tg3MrZA7_F82oqkKiL5yWSQude5rkQAiz");
 
 const mailSender = async (email, title, body) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: "studyhub232@gmail.com",
-        pass: "gjfw lpwu epkk qgnm",
-      },
-    });
-
-    let info = await transporter.sendMail({
-      from: `"StudyNotion" <${"studyhub232@gmail.com"}>`,
+    const data = await resend.emails.send({
+      from: "StudyNotion <sourabhbadgaiya2@gmail.com>", // You can customize sender name & email
       to: email,
       subject: title,
       html: body,
     });
 
-    console.log("Email sent:", info.messageId);
-    return info;
+    console.log("Email sent:", data.id);
+    return data;
   } catch (error) {
-    console.error("Mail send error:", error.message);
+    console.error("Mail send error:", error);
+    return null;
   }
 };
 
-
-module.exports = mailSender;
+export default mailSender;
